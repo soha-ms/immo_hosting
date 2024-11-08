d27da874ddfb11155311156812671e3b8d061562
Soha Al Samaneh <soha.ms84@gmail.com>
2024-11-08 16:29:18 +0100

    deploy streamlit

---
+++ streamlit.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+
+st.title('Simple Streamlit App')
+
+st.write("Here's our first attempt at using Streamlit to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+    st.line_chart(chart_data)
+
+
+    #url = 'https://immo-hosting.onrender.com/hello'
+    #response = requests.post(url)
